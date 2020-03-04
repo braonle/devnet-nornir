132eb31fbe55697b045ca985f1fe01fcb9d02d6d
Iaroslav Bondarenko <iabondar@cisco.com>
2020-03-04 14:57:28 +0100

    MAC address search

--- main.py
+++ main.py
@@ -1,13 +1,63 @@
 from nornir.plugins.tasks import commands
-from nornir.plugins.functions.text import print_result
-from nornir.plugins.tasks.networking import netconf_get, netconf_get_config
-import nornir
-import xml.dom.minidom
+from nornir import InitNornir
+from sys import argv
 
-nr = nornir.InitNornir(config_file="config.yaml")
+mac = None
+if len(argv) != 2:
+    mac = "ca03.15d6.0008"
+else:
+    mac = str(argv[1])
 
-res = nr.run(task=netconf_get, filter_type="subtree", path="<interfaces xmlns='urn:ietf:params:xml:ns:yang:ietf-interfaces'/>")
+nr = InitNornir(config_file="config/config.yaml")
 
-for x in res:
-    dom = xml.dom.minidom.parseString(res[x].result)
-    print(dom.toprettyxml())
+switches = nr.filter(filter_func=lambda x: "switch" in x.groups)
+
+mac_addresses_result = switches.run(task=commands.remote_command, command="sho mac address-table | i DYNAMIC")
+nr.close_connections()
+
+sw_trunks_result = switches.run(task=commands.remote_command, command="sho int trunk | i trunking")
+nr.close_connections()
+
+trunks = {}
+for x in sw_trunks_result:
+    tmp = str(sw_trunks_result[x].result).lstrip(" ").rstrip(" ").splitlines()
+    lst = []
+    for s in tmp:
+        lst.append((s.split())[0])
+    trunks[x] = lst
+
+result = None
+for x in mac_addresses_result:
+    tmp = str(mac_addresses_result[x].result).lstrip(" ").rstrip(" ").splitlines()
+    for s in tmp:
+        words = s.split()
+        if words[1] == mac:
+            intf = words[3]
+            if intf not in trunks[x]:
+                result = [x, intf]
+                break
+    if result is not None:
+        break
+if result is None:
+    sw_interfaces = switches.run(task=commands.remote_command, command="sho inter | i line protocol is|Hardware")
+    nr.close_connections()
+
+    for x in sw_interfaces:
+        tmp = str(sw_interfaces[x].result).lstrip(" ").rstrip(" ").splitlines()
+        intf = ""
+        for strings in tmp:
+            strings = strings.rstrip(" ").lstrip(" ").split()
+            if strings[0] != "Hardware":
+                intf = strings[0]
+            else:
+                if strings[6] == mac:
+                    result = [x, intf]
+                    break
+        if result is not None:
+            break
+
+if result is not None:
+    print(result[0])
+    print(result[1])
+else:
+    print("not found")
